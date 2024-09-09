fc777f40e0b7256c5a8265943d4de61713ca2114
Atticus <atticus.beard@icloud.com>
2024-09-08 18:35:56 -0600

    Just did some minor fixes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.SceneManagement;
-using UnityEditor.SearchService;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
