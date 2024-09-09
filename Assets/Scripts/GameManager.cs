257e7a8da44fed3c5d09316cb3611c2e3b374f80
Atticus <150387049+PortalWalkerCodes@users.noreply.github.com>
2024-09-08 19:23:06 -0600

    Revert "Merging to main"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.SceneManagement;
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
